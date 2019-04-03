3e1245f60f97ceb761949e3b79daf980b4989d29
Heather Cherewaty <hr.cherewaty@gmail.com>
2019-04-03 13:11:21 -0700

    adds ajax call to db for todos

--- public/app.js
+++ public/app.js
@@ -1,2 +1,15 @@
 'use strict';
 
+//wait until DOM is ready
+$(document).ready( () => {
+    $.getJSON('./api/todos')
+    .then(addTodos);
+});
+
+const addTodos = (todos) => {
+    //add todos to the page here -- loop through array of todos and append to the page as a li
+    todos.forEach( todo => {
+        let newTodo = $('<li class="task">' + todo.name + '</li>');
+        $('.list').append(newTodo);
+    });
+}
