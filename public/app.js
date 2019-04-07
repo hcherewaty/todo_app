4efe5ee3057b6d1ba00fd1ae6b527e99a9d9f156
Heather Cherewaty <hr.cherewaty@gmail.com>
2019-04-06 18:13:34 -0700

    udpates

--- public/app.js
+++ public/app.js
@@ -1,84 +1,83 @@
 'use strict';
 
-//wait until DOM is ready
-$(document).ready( () => {
-    $.getJSON("/api/todos")
-    .then(addTodos)
-    
-    //if the keycode hit was 'enter'
-    $('#todoInput').keypress( (event) => {
-      if(event.which === 13) {
-        createTodo();
-      }
-    });
-    
-    $('.list').on('click', 'li', () => {
-      updateTodo($(this));
-    })
-    
-    //root event listener on ul that is present on page load, but listening only on spans within ul with class of 'list'
-    $('.list').on('click', 'span', (event) => {
-      e.stopPropagation();
-      removeTodo($(this).parent());
-    })
-  });
-
-  //add todos to the page here -- loop through array of todos and append to the page as a li
-const addTodos = (todos) => {
-    //add todos to page here
-    todos.forEach( todo => {
-      addTodo(todo);
-    });
-  }
+$(document).ready(function(){
+  $.getJSON("/api/todos")
+  .then(addTodos)
   
-const addTodo = (todo) => {
-    var newTodo = $('<li class="task">' + todo.name + ' <span>X</span></li>');
-    newTodo.data('id', todo._id);
-    newTodo.data('completed', todo.completed);
-    if(todo.completed){
-      newTodo.addClass("done");
+  $('#todoInput').keypress(function(event){
+    if(event.which == 13) {
+      createTodo();
     }
-    $('.list').append(newTodo);
-  }
+  });
   
-const createTodo = () => {
-     //get user input from form
-    let usrInput = $('#todoInput').val();
-    $.post('/api/todos',{name: usrInput})
-    .then( newTodo => {
-      $('#todoInput').val('');
-      addTodo(newTodo);
-    })
-    .catch( err => {
-      console.log(err);
-    })
-  }
+  $('.list').on('click', 'li', function(){
+    updateTodo($(this));
+  })
   
-const removeTodo = (todo) => {
-    let clickedId = todo.data('id');
-    $.ajax({
-      method: 'DELETE',
-      url: `/api/todos/${clickedId}`
-    })
-    .then( data => {
-      todo.remove();
-    })
-    .catch( err => {
-      console.log(err);
-    })
+  $('.list').on('click', 'span', function(e){
+    e.stopPropagation();
+    removeTodo($(this).parent());
+  })
+});
+
+function addTodos(todos) {
+  //add todos to page here
+  todos.forEach(function(todo){
+    addTodo(todo);
+  });
+}
+
+function addTodo(todo){
+  var newTodo = $('<li class="task">'+todo.name +' <span>X</span></li>');
+  newTodo.data('id', todo._id);
+  newTodo.data('completed', todo.completed);
+  if(todo.completed){
+    newTodo.addClass("done");
   }
-  
-const updateTodo = (todo) => {
-    let updateUrl = '/api/todos/' + todo.data('id');
-    let isDone = !todo.data('completed');
-    let updateData = {completed: isDone}
-    $.ajax({
-      method: 'PUT',
-      url: updateUrl,
-      data: updateData
-    })
-    .then( updatedTodo => {
-      todo.toggleClass("done");
-      todo.data('completed', isDone);
-    })
-  }
+  $('.list').append(newTodo);
+}
+
+function createTodo(){
+  //send request to create new todo
+  var usrInput = $('#todoInput').val();
+  $.post('/api/todos',{name: usrInput})
+  .then(function(newTodo){
+    $('#todoInput').val('');
+    addTodo(newTodo);
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+}
+
+function removeTodo(todo){
+  var clickedId = todo.data('id');
+  var deleteUrl = '/api/todos/' + clickedId; 
+  $.ajax({
+    method: 'DELETE',
+    url: deleteUrl
+  })
+  .then(function(data){
+    todo.remove();
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+}
+
+function updateTodo(todo){
+  var updateUrl = '/api/todos/' + todo.data('id');
+  var isDone = !todo.data('completed');
+  var updateData = {completed: isDone}
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+  .then(function(updatedTodo){
+    todo.toggleClass("done");
+    todo.data('completed', isDone);
+  })
+}
+
+
